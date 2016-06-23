72ba62ee05cbc589a77852e1306dd7586cc7ac32
Hack Reactor Students <accounts@hackreactor.com>
2016-06-22 20:39:57 -0700

    Use PARSE to retrieve song list

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,18 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
-
+  model: SongModel,
+  initialize: function() {
+    this.loadSongs()
+  },
+  loadSongs: function() {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/songs/',
+      type: 'GET', 
+      success: response => this.populateSongs(response)
+    });
+  },
+  populateSongs: function(response) {
+    console.log(response.results);
+  }
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -18,6 +18,10 @@ var AppModel = Backbone.Model.extend({
     params.library.on('play', function(song) {
       this.set('currentSong', song);
     }, this);
+
+    params.library.on('enqueue', (song) => {
+      this.get('songQueue').add(song);
+    });
   }
 
 });
