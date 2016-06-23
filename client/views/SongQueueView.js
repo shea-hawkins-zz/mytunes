2b1ca825dde8384bc4998918b313673374cb99d6
Hack Reactor Students <accounts@hackreactor.com>
2016-06-22 19:22:47 -0700

    Make first 1/2 of tests pass.

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,11 +2,16 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.render();
+    this.collection.on('change', this.render, this);
   },
 
   render: function() {
-  initialize: function() {
-  },
+    this.$el.html(this.collection.map(function(song) {
+      var songEntry = new SongQueueEntryView({model: song}); 
+      return songEntry.render();
+    })
+    );
   }
 
 });
